ad2951958ecc1ab1e047fc7de2a36344acf3a6c3
Eugene Ahn <eugeneahn125@gmail.com>
2022-04-06 20:50:20 -0500

    setup database models and began setting up routes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -22,17 +22,24 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      validate: {isDecimal: true}
+      validate: {
+        isDecimal: true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate: {isNumeric: true}
+      validate: {
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
-      // references the Category model's id
+      references: {
+        model: "category",
+        key: "id"
+      }
     }
   },
   {
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,7 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const productData = await Product.findAll().catch((err) => {
+    res.json(err)
+  });
+  res.json(productData)  
   // find all products
   // be sure to include its associated Category and Tag data
 });
